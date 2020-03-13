2ee5fb6266f646d1098aff01f00fced0fab10853
Brian Evans <brianevans99@gmail.com>
2020-03-13 16:23:55 -0400

    setup server.js & package.json for heroku deployment

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const moment = require('moment')
+
+const app = express()
+
+//initialize middleware
+app.use(
+  express.json({
+    extended: false
+  })
+)
+
+//development route *** Comment out or remove upon deployment ***
+app.get('/', (req, res) => {
+  res.send(`${moment().format('LLLL')} - API is running!`)
+})
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
