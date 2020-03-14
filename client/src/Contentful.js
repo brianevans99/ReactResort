f3bb47fc210c369e2eb38f89c7aba535d129a302
Brian Evans <brianevans99@gmail.com>
2020-03-13 21:26:47 -0400

    undo env.development

--- client/src/Contentful.js
+++ client/src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful'
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: '16nee8rwe50d',
+  accessToken: 'BvUsApCt_nrglDOVa2HF99LlOVmDlRE4Pbdd2JgvjBQ'
 })
